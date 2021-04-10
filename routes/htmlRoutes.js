a47a3a8e3d416074617f5fbc2d0e078c8f6ae5ba
bborumoore <bborumoore@gmail.com>
2021-04-10 16:32:25 -0400

    All things up to date

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -12,6 +12,7 @@ module.exports = (app) => {
 
   app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
+    
   });
 
   // If no matching route is found default to home
